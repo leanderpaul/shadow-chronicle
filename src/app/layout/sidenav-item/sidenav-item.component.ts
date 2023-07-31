5b763f686647011d77da6ca97e344f849b86250b
Leander Paul <leanderpaul97@outlook.com>
2023-07-31 21:32:11 +0100

    feat: collapsible navbar

--- src/app/layout/sidenav-item/sidenav-item.component.ts
+++ src/app/layout/sidenav-item/sidenav-item.component.ts
@@ -14,19 +14,12 @@ import { Router, RouterModule } from '@angular/router';
  * Defining types
  */
 
-interface NavItemWithChildren {
+export interface SidenavItem {
   icon: string;
-  name: string;
-  children: NavItemWithLink[];
-}
-
-interface NavItemWithLink {
-  icon?: string;
   name: string;
   link: string;
 }
 
-export type SidenavItem = NavItemWithChildren | NavItemWithLink;
 /**
  * Declaring the constants
  */
@@ -38,41 +31,19 @@ export type SidenavItem = NavItemWithChildren | NavItemWithLink;
   templateUrl: './sidenav-item.component.html',
   styleUrls: ['./sidenav-item.component.scss'],
 })
-export class SidenavItemComponent implements OnInit {
+export class SidenavItemComponent {
   @Input() item: SidenavItem;
-  isOpen: boolean;
-
-  constructor(private readonly router: Router) {}
-
-  ngOnInit(): void {
-    if ('children' in this.item) this.isOpen = this.item.children.some(child => this.isActive(child.link));
-    else this.isOpen = false;
-  }
-
-  private isActive(link?: string): boolean {
-    if (!link) return false;
-    return link.replace(/\/$/, '') === window.location.pathname.replace(/\/$/, '');
-  }
+  @Input() collapsed: boolean;
 
   getFontSet(): string {
     return this.item.icon?.endsWith('_outlined') ? 'material-icons-outlined' : 'material-icons';
   }
 
   getClassList(): string {
-    if ('children' in this.item) return this.isOpen ? 'expanded' : 'collapsed';
-    return this.isActive(this.item.link) ? 'active' : '';
-  }
-
-  handleClick(): void {
-    if ('children' in this.item) this.isOpen = !this.isOpen;
-    else this.router.navigate([this.item.link]);
-  }
-
-  hasChildren(): boolean {
-    return 'children' in this.item;
-  }
-
-  getChildren(): NavItemWithLink[] {
-    return 'children' in this.item ? this.item.children : [];
+    const classList = [];
+    const isActive = this.item.link.replace(/\/$/, '') === window.location.pathname.replace(/\/$/, '');
+    if (isActive) classList.push('active');
+    if (this.collapsed) classList.push('collapsed');
+    return classList.join(' ');
   }
 }
